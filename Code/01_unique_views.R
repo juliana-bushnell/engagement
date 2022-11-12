6e971ee770d5049422b4ebd52763ca7e92e54f29
Qi Yu <yuqiyqq@hotmail.com>
2022-11-11 22:38:52 -0500

    make report work

--- Code/01_unique_views.R
+++ Code/01_unique_views.R
@@ -4,7 +4,8 @@ library(tidyverse)
 library(gtsummary)
 library(ggplot2)
 here::i_am("code/01_unique_views.R")
-configlist<-config::get()
+#configlist<-config::get()
+week_select <- as.numeric(Sys.getenv("WEEK"))
 absolute_file_path<-here::here("course_engagement.csv")
 data <- read.csv(absolute_file_path, header = TRUE)
 #Table
@@ -19,18 +20,20 @@ data1 <-data%>%
   pivot_wider(names_from = video_number, 
               values_from = unique_viewers,names_prefix = "video")
 
-configlist$parameter<-data1%>%slice(configlist$numberparam)
+#configlist$parameter<-data1%>%slice(configlist$numberparam)
+dat_for_table <- data1 %>% slice(week = week_select)
+
 saveRDS(
-  configlist$parameter,
-  file = here::here(paste0("output/week",configlist$numberparam,"_Unique_views_table.rds")
+  dat_for_table,
+  file = here::here(paste0("output/week",week_select,"_Unique_views_table.rds")
 ))
 
 #Visualization
-data<-data%>%filter(week==configlist$numberparam)
+data<-data%>%filter(week==week_select)
 barplot<-ggplot(data, aes(x=video_number, y=unique_viewers)) + 
   geom_bar(stat = "identity")
 ggsave(
-  here::here(paste0("output/week",configlist$numberparam,"_Unique_views_barplot.png")),
+  here::here(paste0("output/week",week_select,"_Unique_views_barplot.png")),
   plot=barplot,
   device = "png"
 )
